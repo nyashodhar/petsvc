0d1c42c5895efb84fd674d2f6fad289d23da51c4
Per Blix <per.blix@mac.com>
2014-09-13 17:31:06 -0700

    Add controller to give 204 response to post-build verification request in CI

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  # DeploymentsController:
+  #===============================
+  get "/", to: "deployments#status", as: 'status'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
