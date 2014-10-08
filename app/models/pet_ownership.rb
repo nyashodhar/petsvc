5cc7f90e795d2bfb7e0f7d5e1d92b6d1b5b85a84
Per Blix <per.blix@mac.com>
2014-10-07 21:04:36 -0700

    Small changes petownership object

--- app/models/pet_ownership.rb
+++ app/models/pet_ownership.rb
@@ -1,3 +1,15 @@
+
+#
+# Switch to a db:
+#   use petsvc_development
+#
+# Show the collections in the current db:
+#   show collections
+#
+# List all the objects in a collection in the current db:
+#   db.pet_ownerships.find()
+#
+
 class PetOwnership
 
   #
@@ -5,10 +17,19 @@ class PetOwnership
   #
   field :user_id, type: Integer
   validates_presence_of :user_id, message: I18n.t("field_is_required")
-  index({ user_id: 1 }, { unique: true, background: true })
+  #index({ user_id: 1 }, { unique: true, background: true })
 
   field :_id, type: String, default: ->{ user_id }
 
-  # TODO: Create 1:N relationship from this object to the pet object.
+  #
+  # PET ID
+  #
+  field :pet_id, type: String
+  validates_presence_of :pet_id, message: I18n.t("field_is_required")
 
+  #
+  # (user_id, pet_id) should be a unique composite key
+  #
+
+  index({ user_id: 1, pet_id: 1 }, { unique: true, background: true })
 end
