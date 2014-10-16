bcbd27f62fe56b7f0c2fe97825d9a3784007ad11
Per Blix <per.blix@mac.com>
2014-10-16 14:40:57 -0700

    Validate breed_bundle_id and creature_type during pet creation

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -7,72 +7,10 @@ class PetsController < AuthenticatedController
   before_action :ensure_owner_of_pet, only: [:update_pet]
 
   #######################################################
-  # Updates an existing pet object.
-  #
-  # 401:
-  #  - Authenticated failed (user not logged in)
-  #  - Authorization failed (the logged in user is not owner of the pet)
-  # 422:
-  #  - One or more of the fields in the pet object were invalid
-  # 500:
-  #  - An unexpected error occurred while updating the pet object
-  #
-  # EXAMPLE LOCAL:
-  # curl -v -X PUT http://127.0.0.1:3000/pet/9d855750-db24-4f15-805b-aaf0309980b9 -H "Accept: application/json" -H "Content-Type: application/json" -H "X-User-Token: e4SnXXxoWd_Kxi67L-xf" -d '{"name":"Fido","birth_year":2012,"creature_type":0,"breed_bundle_id":"dog1","weight_grams":5100}'
-  #######################################################
-  def update_pet
-
-    pet_args = request.params[:pet]
-
-    #
-    # Update the fields in the pet object
-    # Note that it's allowed for the client to only update a subset of all
-    # the fields of a pet in a single request
-    #
-
-    if(!pet_args[:name].blank?)
-      @owned_pet.name = pet_args[:name]
-    end
-
-    if(!pet_args[:birth_year].blank?)
-      @owned_pet.birth_year = pet_args[:birth_year]
-    end
-
-    if(creature_type_and_breed_valid_on_update?(pet_args[:creature_type], pet_args[:breed_bundle_id]))
-    else
-      return
-    end
-
-    if(!pet_args[:breed_bundle_id].blank?)
-      @owned_pet.breed_bundle_id = pet_args[:breed_bundle_id]
-    end
-
-    if(!pet_args[:creature_type].blank?)
-      @owned_pet.creature_type = pet_args[:creature_type]
-    end
-
-    if(!pet_args[:weight_grams].blank?)
-      @owned_pet.weight_grams = pet_args[:weight_grams]
-    end
-
-    begin
-      @owned_pet.save!
-      logger.info "Pet #{@owned_pet.pet_id} was successfully updated by user #{@authenticated_email}:#{@authenticated_user_id}"
-    rescue => e
-      logger.error "Unexpected error when updating pet #{@owned_pet.pet_id}. User #{@authenticated_email}:#{@authenticated_user_id}) - args #{pet_args} - error: #{e.inspect}"
-      render :status => 500, :json => {:error => I18n.t("500response_internal_server_error")}
-      return
-    end
-
-    head 204
-  end
-
-
-  #######################################################
   # Creates a new pet object. When the pet object has
   # successfully created, a pet ownership object is created
-  # to record that the authenticated user is an owner of the
-  # pet that has been created.
+  # to record that the user is an owner of the pet that
+  # has been created.
   #
   # 401:
   #  - Authenticated failed
@@ -90,11 +28,9 @@ class PetsController < AuthenticatedController
 
     pet_args = request.params[:pet]
 
-    #
-    # TODO: Validate the prefix of the breed bundle id with the type of pet.
-    # For example, if the pet is dog, the breed_bundle_id specified must exist
-    # in the dog resource bundle.
-    #
+    if(!creature_type_and_breed_valid_on_create?(pet_args[:creature_type], pet_args[:breed_bundle_id], pet_args))
+      return
+    end
 
     pet = Pet.create(
         pet_id: SecureRandom.uuid,
@@ -106,7 +42,7 @@ class PetsController < AuthenticatedController
     )
 
     #
-    # If the object fails validation, give a 422 error
+    # If the object fails mongoid validation, give a 422 error
     # If anything else fails, give 500 error
     #
 
@@ -143,20 +79,76 @@ class PetsController < AuthenticatedController
       return
     end
 
-    #
-    # TODO: Hmm, is the save needed looks like the doc was created without it?
-    #
-
     begin
       pet_ownership.save!
     rescue => e
-      logger.error "Unexpected error when saving pet ownership for pet (#{@authenticated_email}:#{@authenticated_user_id}), args #{pet_args}, error: #{e.inspect}"
+      logger.error "Unexpected error when saving pet ownership for pet #{pet.pet_id}, user #{@authenticated_email}:#{@authenticated_user_id}, pet_args #{pet_args}, error: #{e.inspect}"
       render :status => 500, :json => {:error => I18n.t("500response_internal_server_error")}
       return
     end
 
-    # Success 201
+    logger.info "Pet #{pet.pet_id} created by user #{@authenticated_email}:#{@authenticated_user_id}"
     render :status => 201, :json => {:id => pet.pet_id, :name => pet.name, :creature_type => pet.creature_type, :breed_bundle_id => pet.breed_bundle_id, :weight_grams => pet.weight_grams}
+  end
+
+
+  #######################################################
+  # Updates an existing pet object.
+  #
+  # 401:
+  #  - Authenticated failed (user not logged in)
+  #  - Authorization failed (the logged in user is not owner of the pet)
+  # 422:
+  #  - One or more of the fields in the pet object were invalid
+  # 500:
+  #  - An unexpected error occurred while updating the pet object
+  #
+  # EXAMPLE LOCAL:
+  # curl -v -X PUT http://127.0.0.1:3000/pet/9d855750-db24-4f15-805b-aaf0309980b9 -H "Accept: application/json" -H "Content-Type: application/json" -H "X-User-Token: e4SnXXxoWd_Kxi67L-xf" -d '{"name":"Fido","birth_year":2012,"creature_type":0,"breed_bundle_id":"dog1","weight_grams":5100}'
+  #######################################################
+  def update_pet
+
+    pet_args = request.params[:pet]
+
+    #
+    # Note that it's allowed for the client to only update a subset of all
+    # the fields of a pet in a single request
+    #
+
+    if(!pet_args[:name].blank?)
+      @owned_pet.name = pet_args[:name]
+    end
+
+    if(!pet_args[:birth_year].blank?)
+      @owned_pet.birth_year = pet_args[:birth_year]
+    end
+
+    if(!creature_type_and_breed_valid_on_update?(pet_args[:creature_type], pet_args[:breed_bundle_id]))
+      return
+    end
+
+    if(!pet_args[:breed_bundle_id].blank?)
+      @owned_pet.breed_bundle_id = pet_args[:breed_bundle_id]
+    end
+
+    if(!pet_args[:creature_type].blank?)
+      @owned_pet.creature_type = pet_args[:creature_type]
+    end
+
+    if(!pet_args[:weight_grams].blank?)
+      @owned_pet.weight_grams = pet_args[:weight_grams]
+    end
+
+    begin
+      @owned_pet.save!
+    rescue => e
+      logger.error "Unexpected error when updating pet #{@owned_pet.pet_id}. User #{@authenticated_email}:#{@authenticated_user_id}) - args #{pet_args} - error: #{e.inspect}"
+      render :status => 500, :json => {:error => I18n.t("500response_internal_server_error")}
+      return
+    end
+
+    logger.info "Pet #{@owned_pet.pet_id} updated by user #{@authenticated_email}:#{@authenticated_user_id}"
+    head 204
   end
 
 end
--- app/helpers/pet_breed_helper.rb
+++ app/helpers/pet_breed_helper.rb
@@ -2,17 +2,81 @@ module PetBreedHelper
 
   ####################################################
   #
-  # Utility operation for pet controller to perform
-  # some custom validation on top of the basic field
-  # validation provided by mongo id.
-  #
   # The values of the fields creature_type and breed_bundle_id
-  # are interdependent, and this method validates that the
-  # values specified during the update of an existing pet are
-  # not incompatible.
+  # are interdependent. This method validates that the
+  # values specified during the creation of a new pet are
+  # valid and compatible.
   #
   # If any validation fails, this method will render a
   # 422 with errors to the client and return false.
+  #
+  # If no problem was found during validation, this
+  # method returns true.
+  #
+  ####################################################
+  def creature_type_and_breed_valid_on_create?(creature_type, breed_bundle_id, pet_args)
+
+    creature_type_missing = creature_type.blank?
+    breed_bundle_id_missing = breed_bundle_id.blank?
+
+    if(creature_type_missing || breed_bundle_id_missing)
+
+      errors_hash = Hash.new
+      if(creature_type_missing)
+        logger.error "creature_type_and_breed_valid_on_create?(): creature_type type was not specified when creating a new pet (pet_args = #{pet_args}, logged in user = #{@authenticated_email}:#{@authenticated_user_id})"
+        errors_hash[:creature_type] = [I18n.t("field_is_required")]
+      end
+
+      if(breed_bundle_id_missing)
+        logger.error "creature_type_and_breed_valid_on_create?(): breed_bundle_id type was not specified when creating a new pet (pet_args = #{pet_args}, logged in user = #{@authenticated_email}:#{@authenticated_user_id})"
+        errors_hash[:breed_bundle_id] = [I18n.t("field_is_required")]
+      end
+
+      render :status => 422, :json => {:error => errors_hash}
+      return false
+    end
+
+    #
+    # Both creature_type and breed_bundle_id is specified.
+    # Give error if the creature_type is not compatible with the breed_bundle_id
+    #
+
+    if(!breed_bundle_id_compatible_with_creature_type?(breed_bundle_id, creature_type))
+      logger.error "creature_type_and_breed_valid_on_create?(): The breed_bundle_id #{breed_bundle_id} is incompatible with the creature type #{creature_type} (pet_args = #{pet_args}, logged in user = #{@authenticated_email}:#{@authenticated_user_id})"
+      errors_hash = {:breed_bundle_id => [I18n.t("breed_bundle_id_incompatible_with_creature_type")], :creature_type => [I18n.t("creature_type_incompatible_with_breed")]}
+      render :status => 422, :json => {:error => errors_hash}
+      return false
+    end
+
+    #
+    # The breed_bundle_id and creature_type are compatible based on prefix.
+    # Final check is to ensure that the breed_bundle_id truly exists in the resource bundle
+    #
+
+    if(!breed_bundle_id_exists_in_resource_bundle?(creature_type, breed_bundle_id))
+      logger.error "creature_type_and_breed_valid_on_create?(): The breed_bundle_id #{breed_bundle_id} was not found in the resource bundle (pet_args = #{pet_args}, logged in user = #{@authenticated_email}:#{@authenticated_user_id})"
+      errors_hash = {:breed_bundle_id => [I18n.t("breed_bundle_id_could_not_be_resolved")]}
+      render :status => 422, :json => {:error => errors_hash}
+      return false
+    end
+
+    return true
+
+  end
+
+
+  ####################################################
+  #
+  # The values of the fields creature_type and breed_bundle_id
+  # are interdependent. This method validates that the
+  # values specified during the update of an existing pet are
+  # valid and compatible.
+  #
+  # If any validation fails, this method will render a
+  # 422 with errors to the client and return false.
+  #
+  # If no problem was found during validation, this
+  # method returns true.
   #
   ####################################################
   def creature_type_and_breed_valid_on_update?(creature_type, breed_bundle_id)
